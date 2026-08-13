a53a935da12ce99daf980bf262a3a3cc1a21d380
Mahmoud_Hassanein <mahmoudmohamedhassanein5@gmail.com>
2026-08-13 06:55:29 +0300

    update AndroidManifest.xml to allow cleartext traffic; fix import path in service_locator.dart; update http dependency in pubspec.yaml; add CloudinaryService for image uploads

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -3,7 +3,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
-import 'injection.config.dart';
+import 'service_locator.config.dart';
 
 final getIt = GetIt.instance;
 
---
+++ lib/core/utils/cloudinary_service.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class CloudinaryService {
+  static const String _cloudName = 'c95zapgj';
+  static const String _uploadPreset = 'medicine_preset';
+
+  static Future<String?> uploadImage(File imageFile) async {
+    try {
+      final url = Uri.parse(
+        'https://api.cloudinary.com/v1_1/$_cloudName/image/upload',
+      );
+
+      final request = http.MultipartRequest('POST', url)
+        ..fields['upload_preset'] = _uploadPreset
+        ..files.add(
+          await http.MultipartFile.fromPath('file', imageFile.path),
+        );
+
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        return responseData['secure_url'] as String?;
+      } else {
+        _log('Cloudinary Upload Failed: ${response.body}');
+        return null;
+      }
+    } catch (e) {
+      _log('Error uploading to Cloudinary: $e');
+      return null;
+    }
+  }
+
+  static void _log(String message) {
+    if (kDebugMode) {
+      log(message, name: 'CloudinaryService');
+    }
+  }
+}
